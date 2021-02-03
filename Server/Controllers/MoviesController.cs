9ffc889639949aee4c35b1169e8996bf3c34b39c
sugu-sano <sugu-sano@i-mobile.co.jp>
2021-02-03 14:49:04 +0900

    改行コードを統一

--- Server/Controllers/MoviesController.cs
+++ Server/Controllers/MoviesController.cs
@@ -1,1 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;using Microsoft.AspNetCore.Mvc;using NetCoreHostedBlazorNetflixClone.Shared.Domain.Models;using System;using System.Threading.Tasks;namespace NetCoreHostedBlazorNetflixClone.Server.Controllers{    [ApiController]    [Route("api/[controller]")]    public class MoviesController : ControllerBase    {        private readonly IMovieRepository movieRepository;        public MoviesController(IMovieRepository movieRepository)        {            this.movieRepository = movieRepository;        }        [HttpGet, Route("{category?}")]        public async ValueTask<ActionResult<MovieEntity[]>> Get([FromRoute] MovieCategory? category)        {            if (!category.HasValue)            {                return this.RedirectToAction(nameof(Get), new { category = MovieCategory.Trending });            }            if (!Enum.IsDefined<MovieCategory>(category.Value))            {                return this.StatusCode(StatusCodes.Status404NotFound);            }            MovieEntity[] movies = await this.movieRepository.ListAsync(category ?? MovieCategory.Trending);            return movies;        }    }}
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreHostedBlazorNetflixClone.Shared.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace NetCoreHostedBlazorNetflixClone.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+
+        [HttpGet, Route("{category?}")]
+        public async ValueTask<ActionResult<MovieEntity[]>> Get([FromRoute] MovieCategory? category)
+        {
+            if (!category.HasValue)
+            {
+                return this.RedirectToAction(nameof(Get), new { category = MovieCategory.Trending });
+            }
+
+            if (!Enum.IsDefined<MovieCategory>(category.Value))
+            {
+                return this.StatusCode(StatusCodes.Status404NotFound);
+            }
+
+            MovieEntity[] movies = await this.movieRepository.ListAsync(category ?? MovieCategory.Trending);
+
+            return movies;
+        }
+    }
+}
